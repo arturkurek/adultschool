59a77bc23f4d00248a70a5b5a54173d8de36e8a5
Artur Kurek <arturkurek10@gmail.com>
2023-06-06 22:06:36 +0200

    woman returned

--- src/views/parts/AboutUs/AboutUs.tsx
+++ src/views/parts/AboutUs/AboutUs.tsx
@@ -5,7 +5,7 @@ import "./AboutUs.scss";
 
 export const AboutUs = () => {
     return <div className='about-us' data-testid="about-us-test-id">
-        {/* <img className="woman" src="/static/images/woman.png" alt="adult students" /> */}
+        <img className="woman" src="/static/images/woman.png" alt="adult students" />
         <div className='motto'>
             <h3>„Jeśli potrafisz o czymś marzyć, to potrafisz także tego dokonać”. ~ Walt Disney</h3>
             <h4><i>If you can dream it, you can do it!</i></h4>
