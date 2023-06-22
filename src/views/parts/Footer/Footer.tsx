5e9e5c015e4c9bce80e8f1985c99edbaa1ca1b3a
szczypiorofix <szczypiorofix@o2.pl>
2023-06-22 08:35:18 +0200

    Add footer

---
+++ src/views/parts/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+
+import "./Footer.scss";
+
+
+export const Footer = () => {
+
+
+    return (
+        <section className="container-full footer">
+            <footer className="container">
+                <div className="footer-content">
+
+                </div>
+            </footer>
+        </section>
+    )
+
+}
+
